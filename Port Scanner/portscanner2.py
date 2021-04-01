bb741bc39d9ef2eafe9f1a5ad6b2e8eb0af7008d
Ansh Vaid <anshvaid4@gmail.com>
2021-04-01 13:03:24 +0530

    done

--- Port Scanner/portscanner2.py
+++ Port Scanner/portscanner2.py
@@ -12,44 +12,47 @@ def scan(targethost,targetport):
 
  
 def Main():
-    parser=optparse.OptionParser("Usage => -t for hostname -p for port number")
+    parser=optparse.OptionParser("Usage => -t for hostname -p for port number -i for IP")
     parser.add_option("-t", dest="hname", type="string",help="Enter the hostname")
+    parser.add_option("-i", dest="ip", type="string",help="Enter the IP address")
     parser.add_option("-p", dest="pname", type="string",help="Enter the port number")
     (options,args)=parser.parse_args()
-    if ((options.hname == None) | (options.pname == None)):
+    if (((options.hname == None) & (options.pname == None)) | ((options.ip == None) & (options.pname == None))):
         print(parser.usage)
         exit(0)
-    else:
-        target=options.hname
-        print("[+] Starting scan for "+target+"\n")
-        if "." in target:
-            #name,alias,addresslist = lookup(target)
-            try:
-                hostName=gethostbyaddr(target)
-                print("[+] Host name resolved to "+hostName[0])
-            except:
-                hostName="Not Resolved"
-                print("[-] Host name  "+hostName)
-            finally:
-                if '-' in options.pname:
-                    portRange=options.pname.split("-")
-                    portRange=range(int(portRange[0]),int(portRange[1]))
-                else:
-                    portRange=str(options.pname).split(",") 
-                for port in portRange:
-                     port=str(port)
-                     scan(target,port)
+    if (not (options.hname == None)):
+        host=options.hname
+        print("[+] Starting scan for "+host+"\n")
+        target=gethostbyname(host)
+        print("[+] Target IP found "+target)
+        setdefaulttimeout(1)
+        if '-' in options.pname:
+            portRange=options.pname.split("-")
+            portRange=range(int(portRange[0]),int(portRange[1]))
         else:
-            target=gethostbyname(target)
-            print("[+] Target IP found "+target)
+            portRange=str(options.pname).split(",") 
+        for port in portRange:
+            port=str(port)
+            scan(target,port)
+    else:
+        target=options.ip
+        #name,alias,addresslist = lookup(target)
+        setdefaulttimeout(1)
+        try:
+            hostName=gethostbyaddr(target)
+            print("[+] Host name resolved to "+hostName[0])
+        except:
+            hostName="Not Resolved"
+            print("[-] Host name  "+hostName)
+        finally:
             if '-' in options.pname:
                 portRange=options.pname.split("-")
                 portRange=range(int(portRange[0]),int(portRange[1]))
             else:
                 portRange=str(options.pname).split(",") 
             for port in portRange:
-                port=str(port)
-                scan(target,port)
+                 port=str(port)
+                 scan(target,port)
             
 print("\n\n")
 print("||\          |                                                                                               ")
